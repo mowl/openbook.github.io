b53e6843ad039379c0ec49adaccdda58a67456b4
mowl <mowl@live.nl>
2014-05-10 01:41:41 +0200

    Add: Template.js - a very minimalistic templating system to easily parse data wrapped in so called 'cards'. Add: Cards.js - renders two cards with test data, as example
    Fix: Resources are properly added this time.
    
    Ref: Splitted resources as helpers.

--- application/config/resources.php
+++ application/config/resources.php
@@ -12,12 +12,20 @@ $config['css'] = array(
     array(
         'file' => 'http://fonts.googleapis.com/css?family=Lato:300,400,700',
         'extern' => true
+    ),
+    array(
+        'file' => 'http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css',
+        'extern' => true
     )
 );
 
 $config['js'] = array(
     array(
         'file' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js',
+        'extern' => true
+    ),
+    array(
+        'file' => 'http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js',
         'extern' => true
     ),
     array(
--- application/views/components/navbar_user_view.php
+++ application/views/components/navbar_user_view.php
@@ -8,11 +8,29 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
+            
             <a class="navbar-brand" href="<?php echo base_url(); ?>">OpenBook</a>
         </div>
 
         <div class="collapse navbar-collapse" id="openBookNav">
-            
+
+            <ul class="nav navbar-nav navbar-right">
+                
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">mowl <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Settings</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </li>
+                
+                <li class="user-icon">
+                    <i class="fa fa-user"></i>
+                </li>
+                
+            </ul>
+
         </div>
     </div>
 </nav>
