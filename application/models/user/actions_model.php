3e35794a4770694ca4bde4fa686ded67f38452d8
mowl <mowl@live.nl>
2014-05-11 20:11:55 +0200

    Cleanup: Basically restructured several vital parts such as: - The user actions renamed to 'user_api' in a folder named 'api', this will control all internal api calls - user/actions_model renamed to user_model

--- application/models/user/actions_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Actions_model extends CI_Model {
-    
-    function __construct() {
-        parent::__construct();
-    }
-}
--- application/views/components/navbar_user_view.php
+++ application/views/components/navbar_user_view.php
@@ -14,7 +14,6 @@
 
         <div class="collapse navbar-collapse" id="openBookNav">
 
-            <!-- Should be displayed for only logged in user -->
             <ul class="nav navbar-nav navbar-right">
                 
                 <li class="dropdown">
